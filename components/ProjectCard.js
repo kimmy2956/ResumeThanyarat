762c07ec14764daebc721869643927c3b7d64b9f
Thanyarat Phapachiratsakul <boxshoesshopping@gmail.com>
2025-10-23 15:36:43 +0700

    Update ProjectCard.js

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -3,7 +3,7 @@ export default function ProjectCard({ project }) {
     <article className="p-4 border rounded hover:shadow">
       <h3 className="font-semibold">{project.title}</h3>
       <p className="text-sm text-slate-600 mt-1">{project.desc}</p>
-      <a href={project.link} className="mt-3 inline-block text-sm">View →</a>
+      <a href={project.link} className="mt-3 inline-block text-sm">ดูข้อมูลเพิ่มเติม →</a>
     </article>
   )
 }
