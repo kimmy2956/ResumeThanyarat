3db418c41b35a590ceb7ea6cde218be739f8c599
Thanyarat Phapachiratsakul <boxshoesshopping@gmail.com>
2025-10-23 15:17:30 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="border-t mt-12">
-      <div className="container py-6 text-sm text-slate-600">© {new Date().getFullYear()} ชื่อของคุณ — Built with Next.js</div>
+      <div className="container py-6 text-sm text-slate-600">© {new Date().getFullYear()} Thanyarat Phapachiratsakul — Built with Next.js</div>
     </footer>
   )
 }
