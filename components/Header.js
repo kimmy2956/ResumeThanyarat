3d1dd52064aa7a8089404b5f002a5f4a49738f71
Thanyarat Phapachiratsakul <boxshoesshopping@gmail.com>
2025-10-24 12:42:12 +0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,10 +2,9 @@ export default function Header() {
   return (
     <header className="border-b">
       <div className="container py-4 flex items-center justify-between">
-        <div className="font-bold">kimmy resume</div>
+        <div className="font-bold">Resume</div>
         <nav className="hidden md:flex gap-4">
-          <a href="#about" className="text-sm">เกี่ยวกับฉัน</a>
-          <a href="#projects" className="text-sm">โปรเจคที่ผ่านมา</a>
+          <a href="#about" className="text-sm">ข้อมูลการศึกษา</a>
           <a href="#contact" className="text-sm">การติดต่อ</a>
         </nav>
       </div>
